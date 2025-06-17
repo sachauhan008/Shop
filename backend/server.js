0f2f2997b27fbd7fee7faaf3c1efbae8f9888b0c
“[sunil <[sachauhan008@gmail.com]>
2025-06-17 14:27:03 +0530

    add file

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on port 5000'));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => console.log(err));
